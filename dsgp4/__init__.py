0f97003d76fc883786eaa88018f7c45761850571
Sceki <giacomo.acciarini@gmail.com>
2023-10-17 17:38:07 +0000

    Deploying to gh-pages from @ esa/dSGP4@48b6a1cc5fa824d1668362b187529d0bdb895aa8 🚀

--- dsgp4/__init__.py
+++
@@ -1,7 +0,0 @@
-__version__ = '0.0.1'
-
-from .sgp4 import sgp4
-from .initl import initl
-from .sgp4init import sgp4init
-from .newton_method import newton_method, update_TLE
-from .sgp4_batched import sgp4_batched
