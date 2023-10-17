20a47cecaa0a02df71b332f514b2a099c012861f
Giacomo Acciarini <giacomo.acciarini@gmail.com>
2023-10-17 16:12:05 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -35,7 +35,6 @@ release = dsgp4.__version__
 extensions = ["myst_nb", "sphinx.ext.intersphinx"]
 
 intersphinx_mapping = {
-    "dsgp4": ("https://esa.github.io/dSGP4/", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
     "python": ("https://docs.python.org/3", None),
     "matplotlib": ("https://matplotlib.org/stable/", None),
