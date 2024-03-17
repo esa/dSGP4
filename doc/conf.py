9167d376cd5c7350dc3eb49df75c736dab32ec4d
Sceki <giacomo.acciarini@gmail.com>
2024-03-17 19:02:32 +0100

    exclude notebook with plot

--- doc/conf.py
+++ doc/conf.py
@@ -81,7 +81,8 @@ html_theme_options = {
 nb_execution_mode = "force"
 
 nb_execution_excludepatterns = [
-    "gradient_based_optimization.ipynb"
+    "gradient_based_optimization.ipynb",
+    "tle_propagation.ipynb"
 ]
 
 latex_engine = "xelatex"
