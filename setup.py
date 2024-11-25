c738cf8c9fba679462ae9004739f1972942631f1
Acciarini, Giacomo (PG/R - Maths & Physics) <g.acciarini@surrey.ac.uk>
2024-11-25 10:07:38 +0100

    workflows

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 from setuptools import setup, find_packages
 PACKAGE_NAME = 'dsgp4'
-MINIMUM_PYTHON_VERSION = 3, 10
+MINIMUM_PYTHON_VERSION = 3, 8
 
 with open('README.md', 'r') as f:
     long_description = f.read()
